8767396a665b718f6c4b97d7bd76fc3ff7372c11
Jaimin-Bacancy <jaimin.parmar@bacancy.com>
2021-04-19 12:05:44 +0530

    final changes

--- main.go
+++ main.go
@@ -19,7 +19,6 @@ type Customer struct {
 
 //connection with postgres database
 func getconnection() *sql.DB {
-
 	databasename := "Customer"
 	database := "postgres"
 	databasepassword := "1312"
@@ -42,7 +41,6 @@ func getconnection() *sql.DB {
 
 //Insert customer to database
 func insertCustomer() {
-
 	db := getconnection()
 	defer db.Close()
 
@@ -65,7 +63,6 @@ func insertCustomer() {
 
 //Get all customer from database
 func getallCustomer() {
-
 	db := getconnection()
 	defer db.Close()
 
@@ -86,9 +83,30 @@ func getallCustomer() {
 
 }
 
+//Update one customer to database using updateCustomerId
+func updateCustomer(updateCustomerId string) {
+	db := getconnection()
+	defer db.Close()
+
+	query := "UPDATE customer SET firstname = $1,lastname = $2,email = $3 ,dateofbirth =$4 ,mobilenumber =$5 where id = $6"
+	customer := Customer{
+		Customerid:   updateCustomerId,
+		FirstName:    "John",
+		LastName:     "Doe",
+		Email:        "JohnDoe1312@gmail.com",
+		Dateofbirth:  "13 December 1999",
+		Mobilenumber: "5633234443",
+	}
+	_, err := db.Exec(query, customer.FirstName, customer.LastName, customer.Email, customer.Dateofbirth, customer.Mobilenumber, customer.Customerid)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println("Customer data updated successfully into database")
+}
+
 //Delete one customer to database using deleteCustomerId
 func deleteCustomer(deleteCustomerId string) {
-
 	db := getconnection()
 	defer db.Close()
 
@@ -111,34 +129,13 @@ func deleteCustomer(deleteCustomerId string) {
 	log.Println("Customer data deleted successfully into database")
 }
 
-//Update one customer to database using updateCustomerId
-func updateCustomer(updateCustomerId string) {
-
-	db := getconnection()
-	defer db.Close()
-
-	query := "UPDATE customer SET firstname = $1,lastname = $2,email = $3 ,dateofbirth =$4 ,mobilenumber =$5 where id = $6"
-	customer := Customer{
-		Customerid:   updateCustomerId,
-		FirstName:    "John",
-		LastName:     "Doe",
-		Email:        "JohnDoe1312@gmail.com",
-		Dateofbirth:  "13 December 1999",
-		Mobilenumber: "5633234443",
-	}
-	_, err := db.Exec(query, customer.FirstName, customer.LastName, customer.Email, customer.Dateofbirth, customer.Mobilenumber, customer.Customerid)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	log.Println("Customer data updated successfully into database")
-}
-
 func main() {
 	insertCustomer()
 	getallCustomer()
-	updateCustomer("1")
+
+	updateCustomer("1") //pass argument customerid here: 1
 	getallCustomer()
-	deleteCustomer("1")
+
+	deleteCustomer("1") //pass argument customerid here: 1
 	getallCustomer()
 }
